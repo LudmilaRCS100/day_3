7db5bc5c35277df435467263f0862994e9ba3f0c
Ludmila <luvi_lv@inbox.lv>
2018-06-11 21:08:08 +0300

    step2

---
+++ main.js
@@ -0,0 +1,47 @@
+alert('Hello, world!');
+var someString1='foo';//string
+var someString2='foo';
+var someString3='foo';
+
+var somIntenger = 123;//integer
+var somFloat = 3.14;//
+var somBool = true;//boolean
+var somCars = ['VAZ', 'BMW', 123, 3.14];//
+
+var someNull = null;//
+var somePerson = {
+    name: 'Jonny',
+    surname: 'Gates',
+    age:26,
+
+}
+var people=[
+    {
+        name: 'Jonny',
+        surname: 'Gates',
+        age:26
+    },
+    {
+        name: 'Jonny',
+        surname: 'Gates',
+        age:26
+    }
+    
+];
+var doSomeThing = function(){
+
+}
+// + - * / %
+var first=1;
+var second = "1";
+first + second;
+
+function doSomeOtherThing(first, second) {
+    return first + second;
+
+}
+parseFloat("3,13")
+function soStuff(someText) {
+    alert("The text is" + someText)
+}
+
